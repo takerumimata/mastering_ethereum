1510b443e6a675541bf36b842db01e0725c778ac
takerumimata <fossil1908@gmail.com>
2020-10-02 21:55:33 +0900

    complete: 2.7

---
+++ Faucet.sol
@@ -0,0 +1,17 @@
+// 最初のコントラクト
+contract Faucet {
+
+    // 要求する人にetherを与える
+    function withdraw(uint withdraw_amount) public {
+
+        // 引出額を制限する
+        require(withdraw_amount <= 10000000000000);
+
+        // リクエストしたアドレスにその全額のetherを送金する
+        msg.sender.transfer(withdraw_amount);
+    }
+
+    // 入金額を受け入れる
+    function () public payable {}
+    
+}
