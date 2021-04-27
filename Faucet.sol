add8be1d81d3e5d4a28406570149d6566f18d6a9
takerumimata <fossil1908@gmail.com>
2021-04-27 21:21:15 +0900

    define constructor and destructor

--- Faucet.sol
+++ Faucet.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.4.19;
 // 最初のコントラクト
 contract Faucet {
 
+    address owner;
+
+    // コントラクトの初期化
+    constructor() {
+        owner = msg.sender;
+    }
+
     // 要求する人にetherを与える
     function withdraw(uint withdraw_amount) public {
 
@@ -16,5 +23,8 @@ contract Faucet {
     // 入金額を受け入れる
     function () public payable {}  //default関数、またはcallback関数
     // Contractが呼ばれた時に、メソッド等の指定がなかった時に走る
-    
+    function destroy() public {
+        require(msg.sender == owner);
+        selfdestruct(owner);
+    }
 }
